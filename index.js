46d7d6f75d70c57acb281bd89ec1df6f967e06fd
Janara Tolonbaeva <janaratolonbaeva@gmail.com>
2024-03-08 12:20:38 +0600

    Added homework 1

---
+++ index.js
@@ -0,0 +1,32 @@
+// alert('Hello World!');
+// const year = prompt('How old are you?', '');
+// alert('I am ' + year);
+
+// const areYourDev = confirm('Are you web Dev?');
+// alert(areYourDev);
+
+// let nameDev = 'Janara';
+
+// alert('I am ' + nameDev);
+
+// nameDev = 'Nurgen';
+
+// alert('And now I am ' + nameDev);
+
+// const a = 2;
+// const b = 4;
+// const c = 2;
+// const total = (a + b) / c;
+// alert(total);
+
+const firstname = 'jAnara';
+const position = 'Web Dev';
+
+// alert('firstname-дин узунудугу: ' + firstname.length + ' тамгадан');
+// alert(position.slice(0, 3));
+// alert('firstname-ди кичине тамгадан жазабыз ' + firstname.toLowerCase());
+const string = 'эЙ, жАнара, КаНдАйСыН?';
+const stringRight =
+  string[0].toUpperCase() + string[1].toLowerCase() + ', ' + string[4].toUpperCase() + string.slice(5).toLowerCase();
+
+alert(stringRight);
